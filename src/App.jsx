59f309c39e5b8dcec511108518472d72c40fbade
Rasheed Mudasiru <rasheedrtm1@gmail.com>
2026-02-25 23:30:34 +0100

    fix(router): add basename for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Auth from './pages/Auth';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<Auth />} />
